d9665d6bf0f28336a096551c3af3a8ddb91e48aa
lizijie <562949697@qq.com>
2019-02-24 18:05:53 +0800

    update

---
+++ react-redux-router/src/reducers.js
@@ -0,0 +1,14 @@
+//export default 只导出一个
+// const tiger=0;
+function couter (state,action){
+        switch(action.type){
+            case "INCREMENT":
+            return state +1
+            case "DECREMENT":
+            return  state -1
+            default:
+            return state
+        }
+    }
+
+export {couter}
