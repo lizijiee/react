a87840bb64041ed85cacf1ce691896548fca0df7
lizijiee <562949697@qq.com>
2019-10-15 15:57:37 +0800

    modify Vue element and miss #vue Bug

--- 微前端/self-complex-example/project1/src/pages/page2.js
+++ 微前端/self-complex-example/project1/src/pages/page2.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class HomePage extends React.Component {
     render () {
         return (
-            <h1>Page2 !</h1>
+            <h1>我是React  Page2 !</h1>
         )
     }
 }
--- 微前端/self-complex-example/project2/src/App.vue
+++ 微前端/self-complex-example/project2/src/App.vue
@@ -1,24 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="vue">
+    <img alt="Vue logo" src="http://localhost:8237/img/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import logo from "./assets/logo.png";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+#vue {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
