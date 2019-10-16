03fe4ca33243126d9906b7099afd1edc5e786bf6
lizijiee <562949697@qq.com>
2019-10-16 09:59:04 +0800

    null

--- 微前端/single-spa-portal-example-master/src/config.js
+++
@@ -1,8 +0,0 @@
-import * as isActive from './activityFns.js'
-import * as singleSpa from 'single-spa'
-
-singleSpa.registerApplication('navbar', () => SystemJS.import('@portal/navbar'), isActive.navbar)
-singleSpa.registerApplication('people', () => SystemJS.import('@portal/people'), isActive.people)
-singleSpa.registerApplication('planets', () => SystemJS.import('@portal/planets'), isActive.planets)
-
-singleSpa.start()
